fd2f49c83d67718e90eeba80dba474f6f2f0e1e5
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-08-31 19:09:13 -0300

    first commit

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/components/category_item.dart';
+import 'package:meals_app/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Vamos Cozinhar?')),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
+    );
+  }
+}
