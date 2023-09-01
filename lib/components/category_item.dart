d6b4f29474496d4fc8c02fa91077d1874a48da9e
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-09-01 09:25:53 -0300

    working with routes

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
+import 'package:meals_app/screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem(this.category, {super.key});
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (_) {
+        return CategoriesMealsScreen(category);
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.bodyMedium,
         ),
       ),
-      child: Text(category.title),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      darkTheme: ThemeData.dark(),
       title: 'DeliMeals',
       theme: ThemeData(
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
         primarySwatch: Colors.pink,
+        fontFamily: 'Raleway',
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          titleLarge: TextStyle(fontSize: 25),
+          bodyMedium: TextStyle(fontSize: 14, fontFamily: 'RobotoCondensed'),
+        ),
       ),
       home: CategoriesScreen(),
     );
