13d89ea0a83a02d295b83f7e688b3336850c4dba
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-09-01 12:20:37 -0300

    categories created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'screens/categories_meals_screen.dart';
 import 'screens/categories_screen.dart';
+import 'utils/app_routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,19 +10,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      darkTheme: ThemeData.dark(),
-      title: 'DeliMeals',
-      theme: ThemeData(
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        primarySwatch: Colors.pink,
-        fontFamily: 'Raleway',
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          titleLarge: TextStyle(fontSize: 25),
-          bodyMedium: TextStyle(fontSize: 14, fontFamily: 'RobotoCondensed'),
+        darkTheme: ThemeData.dark(),
+        title: 'DeliMeals',
+        theme: ThemeData(
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          primarySwatch: Colors.pink,
+          fontFamily: 'Raleway',
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            titleLarge: TextStyle(fontSize: 25),
+            bodyMedium: TextStyle(fontSize: 14, fontFamily: 'RobotoCondensed'),
+          ),
         ),
-      ),
-      home: CategoriesScreen(),
-    );
+        routes: {
+          AppRoutes.HOME: (ctx) => CategoriesScreen(),
+          AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        });
   }
 }
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
 
 import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  final Category category;
-
-  const CategoriesMealsScreen(this.category, {super.key});
-
   @override
   Widget build(BuildContext context) {
+    final category = ModalRoute.of(context)?.settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Receitas'),
+        title: Text(category.title),
       ),
-      body: Center(
-        child: Text(
-          'Receitas por Categoria ${category.title}.',
-        ),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
