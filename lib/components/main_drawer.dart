fe0b0783c472eed8b72a4ad4c2af7755fe87df97
Gabriel <gabriel.merigo.dev@gmail.com>
2023-11-28 14:21:06 -0300

    create item func done!

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class MainDrawer extends StatelessWidget {
+  Widget _createItem(IconData icon, String label) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        label,
+        style: const TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(children: [Container(child: Text('Vamos Cozinhar?'))]),
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            color: Theme.of(context).highlightColor,
+            alignment: Alignment.bottomCenter,
+            child: const Text(
+              'Vamos Cozinhar?',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          _createItem(Icons.restaurant, 'Restaurante'),
+          _createItem(Icons.settings, 'Configurações')
+        ],
+      ),
     );
   }
 }
